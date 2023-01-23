5e384340c674c9ef4557c5c11cbf80670fa9a665
Said Gadjiev <ackusha1984@ya.ru>
2023-01-24 00:12:11 +0300

    fix 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<String> list = new List<String>();
+list.Add(Console.ReadLine());
+list.Add("world");
+list.Add("!");
+Console.WriteLine(list[0]);
+
+
+// Ввести размер массива
+
+// int CorrectNumber(string message)
+// {
+//     bool isCorrect = false;
+//     int result = 0;
+//     while(! isCorrect)
+//     {
+//         Console.Write(message);
+//         if (int.TryParse(Console.ReadLine(), out result)) isCorrect = true;
+//         else Console.WriteLine("Enter correct number");
+//     }
+//     return result;
+// }
+
+// // Получить массив из случайных чисел
+
+// Array[] InitArray(int num)
+// {
+//     List<String> list = new List<String>();
+//     Console.Write("[");
+//     for (int i = 0; i < num; i++)
+//     {
+//         list.Add(Console.ReadLine());
+//     }
+//     Console.Write("]");
+//     return list;
+// }
+
+
+// int num = CorrectNumber("Enter a number -> ");
+// Array[] array = InitArray(num);
