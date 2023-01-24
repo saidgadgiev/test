187bcbf1c7826fc13644f551be17754972d2e01e
SaidFlay <ackusha_dargo@bk.ru>
2023-01-24 16:07:10 +0300

    Порядок

---
+++ C#/Program.cs
@@ -0,0 +1,45 @@
+﻿// Ввести размер массива
+
+int CorrectNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+    while(! isCorrect)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result)) isCorrect = true;
+        else Console.WriteLine("Enter correct number");
+    }
+    return result;
+}
+
+// Получить массив из случайных чисел
+
+String[] InitArray(int num)
+{
+    string[] list = new string[num];
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"Введите {i+1}-й элемент массива -> ");
+        list[i] = Console.ReadLine();
+        // Console.WriteLine(list[i]);
+    }
+    // Console.Write("]");
+    return list;
+}
+
+// Печать массива
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + (i < array.Length-1 ?", ":""));
+    }
+    Console.Write("]");
+}    
+
+int num = CorrectNumber("Введите длинну строкового массива не больше 10 -> ");
+string[] array = InitArray(num);
+PrintArray(array);
---
+++ FinalProject.py
@@ -0,0 +1,25 @@
+import random
+#  Создание массива
+def Add_Array(num):
+    array=[]
+    print(f"Будет создан массив из {num} элементов")
+    print("*"*20)
+    for i in range (num):
+        # print(i)
+        text= input(f"Введите {i+1} -й элемент массива и нажмите Enter-> ")
+        array.append(text)
+    print("*"*20)
+    print(f"Создан массив из {num+1} элементов")
+    print("*"*10)
+    return array
+
+
+num_rand = random.randint(3,10)
+# Add_Array()
+list = Add_Array(num_rand)
+print(list)
+print("*"*20)
+
+number_rand = random.randint(0,3)
+print(f"Из имеющего массива формируеться массив из строк,\n длина которого равно {number_rand} символам")
+print(random.sample(list, number_rand))
